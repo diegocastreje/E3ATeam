f3d60b5a46d597c938d92f5a71aa92c209914cf4
francisco.novo <fnovor@fpcoruna.afundacion.org>
2021-05-18 12:34:05 +0200

    Creada prueba ramas limpias

---
+++ ProyectoPruebaRamasLimpio/src/main/java/com/example/ProyectoPruebaRamasLimpio/Alumno.java
@@ -0,0 +1,15 @@
+package com.example.ProyectoPruebaRamasLimpio;
+
+public class Alumno {
+
+    private String getNombreAlumno;
+
+    private String getApellido1Alumno;
+
+    private String getApellido2Alumno;
+
+    private int getEdadAlumno;
+
+    private String centroProcedenciaAlumno;
+    
+}
