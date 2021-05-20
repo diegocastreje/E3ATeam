9286bceb12c7eb6b96d59fe0ec7cb093880f8dca
leandrotd <leandrotulian45@gmail.com>
2021-05-20 13:01:42 +0200

    Inicio proyecto de MasQuePerros
    Se han creados los proyectos de back y de front.
    Este es el commit inicial.

---
+++ MasQuePerros/MasQuePerrosBack/src/main/java/com/e3a/MasQuePerrosApplication.java
@@ -0,0 +1,13 @@
+package com.e3a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasQuePerrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasQuePerrosApplication.class, args);
+	}
+
+}
