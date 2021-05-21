e27c9d3c0bda40a6847112c9020c86ad53eaeb59
Niqui <hectorniqui96@gmail.com>
2021-05-21 14:13:03 +0200

    este no va el siguiente si

--- MasQuePerros/MasQuePerrosBack/src/main/java/com/e3a/controllers/UserRestController.java
+++ MasQuePerros/MasQuePerrosBack/src/main/java/com/e3a/controllers/UserRestController.java
@@ -3,11 +3,17 @@ package com.e3a.controllers;
 import com.e3a.models.entity.User;
 import com.e3a.models.services.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -19,6 +25,29 @@ public class UserRestController {
     @GetMapping("/users")
     public List<User> index() {
         return userService.findAll();
-
     }
+    
+    
+   /*
+	@GetMapping("/users/{id}")
+	public ResponseEntity<?> show(@PathVariable Long id) {
+		
+		User user = null;
+		Map<String , Object> response = new HashMap();
+		
+		try {
+			user = userService.findById(id);
+		}catch (DataAccessException e) {
+			response.put("mensaje","Error al realizar la consulta en la base de datos");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		if(user==null) {
+			response.put("mensaje", "El cliente ID: ".concat(id.toString().concat(" no existe en la base de datos!")));
+			return new ResponseEntity<Map>(response, HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<User>(user, HttpStatus.OK); 
+	}
+    */
 }
