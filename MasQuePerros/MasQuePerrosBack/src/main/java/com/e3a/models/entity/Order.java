d24b23f3f9f137c5a84e31c53882ba762ca9872c
Zeon <diegocastreje11@gmail.com>
2021-05-21 09:21:50 +0100

    Creadas clases Order, Item y OrderItems

---
+++ MasQuePerros/MasQuePerrosBack/src/main/java/com/e3a/models/entity/Order.java
@@ -0,0 +1,79 @@
+package com.e3a.models.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="orders")
+public class Order implements Serializable{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long order_id;
+	
+	@JsonIgnoreProperties(value = {"orders","hibernateLazyInitializer","handler"}, allowSetters = true)
+	@ManyToOne(fetch = FetchType.LAZY)
+	private User user;
+	
+	private double price;
+	 
+	@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name="order_id")
+	private List<OrderItem> items;
+
+	public Order(long order_id, User user, double price, List<OrderItem> items) {
+		super();
+		this.order_id = order_id;
+		this.user = user;
+		this.price = price;
+		this.items = items;
+	}
+
+	public long getOrder_id() {
+		return order_id;
+	}
+
+	public void setOrder_id(long order_id) {
+		this.order_id = order_id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public List<OrderItem> getItems() {
+		return items;
+	}
+
+	public void setItems(List<OrderItem> items) {
+		this.items = items;
+	}
+	
+	private static final long serialVersionUID = 1L;
+}
