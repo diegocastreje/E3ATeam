0a725055c65242c6898916482d85bd0b095a947c
leandrotd <leandrotulian45@gmail.com>
2021-05-21 12:23:07 +0200

    Agregado el component User + listado basico
    Se ha creado el componente User y en el HTML se ha puesto una tabla basica para comprobar su funcionamiento pero todavia no está probado por la falta de desarrollo en back

--- MasQuePerros/MasQuePerrosFront/src/app/app.module.ts
+++ MasQuePerros/MasQuePerrosFront/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { UserComponent } from './users/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
