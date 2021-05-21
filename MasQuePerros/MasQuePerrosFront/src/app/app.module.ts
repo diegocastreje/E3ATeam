099bf0ff76a0c6a5da49c97ecc8caf089da79ce7
leandrotd <leandrotulian45@gmail.com>
2021-05-21 13:31:31 +0200

    Agregadas las librerías al AppModule
    Se han agregado las librerías RouterModule, Routes y HttpClientModule, además de agregarlos a imports y crear el array de rutas

--- MasQuePerros/MasQuePerrosFront/src/app/app.module.ts
+++ MasQuePerros/MasQuePerrosFront/src/app/app.module.ts
@@ -1,24 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HttpClientModule } from '@angular/common/http';
 import { FooterComponent } from './footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UserComponent } from './users/user.component';
 
+const routes: Routes = [];
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    UserComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, UserComponent],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
