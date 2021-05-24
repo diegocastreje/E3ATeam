f5c5749fc6694286911a3bc0d5f8984de1b1a2f2
francisco.novo <fnovor@fpcoruna.afundacion.org>
2021-05-24 09:00:07 +0200

    Creado método show y manejo de errores

--- MasQuePerros/MasQuePerrosBack/src/main/java/com/e3a/controllers/UserRestController.java
+++ MasQuePerros/MasQuePerrosBack/src/main/java/com/e3a/controllers/UserRestController.java
@@ -3,12 +3,16 @@ package com.e3a.controllers;
 import com.e3a.models.entity.User;
 import com.e3a.models.services.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -24,8 +28,14 @@ public class UserRestController {
     }
 
     @GetMapping("/users/{id}")
-    public User show(@PathVariable Long id) {
-        return userService.findById(id);
+    public ResponseEntity<?> show(@PathVariable Long id) {
+        User user = userService.findById(id);
+        Map<String, Object> response = new HashMap<>();
+        if(user == null) {
+            response.put("mensaje", "El usuario ID:".concat(id.toString().concat(" no existe en la base de dattos.")));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<User>(user, HttpStatus.OK);
     }
-    
+
 }
