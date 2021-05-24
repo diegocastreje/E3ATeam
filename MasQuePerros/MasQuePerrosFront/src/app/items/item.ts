38dfe60f48dd57498aaa41c10f0faba399abbcee
Zeon <diegocastreje11@gmail.com>
2021-05-24 17:59:36 +0100

    Adaptación del component de usuarios a productos

---
+++ MasQuePerros/MasQuePerrosFront/src/app/items/item.ts
@@ -0,0 +1,9 @@
+export class Item {
+  id: number = 0;
+  name: string = "";
+  description: string = "";
+  amount: number = 0;
+  picture: string = "";
+  price: number = 0;
+  category: string = "";
+}
