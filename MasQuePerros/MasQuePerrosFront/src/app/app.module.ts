56210230c7a892f1093f8ee5a29c583c63d7cb0f
Zeon <diegocastreje11@gmail.com>
2021-05-24 11:53:39 +0100

    Creacion header y nav

--- MasQuePerros/MasQuePerrosFront/src/app/app.module.ts
+++ MasQuePerros/MasQuePerrosFront/src/app/app.module.ts
@@ -2,17 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ProductosComponent } from './productos/productos.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/productos', pathMatch: 'full'},
+  {path: 'productos', component: ProductosComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ProductosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
