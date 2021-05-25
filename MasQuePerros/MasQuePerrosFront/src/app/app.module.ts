6069dba718122ef23c74cfea58c8cb4c2509b3e1
Zeon <diegocastreje11@gmail.com>
2021-05-25 10:07:38 +0100

    Fin de productos

--- MasQuePerros/MasQuePerrosFront/src/app/app.module.ts
+++ MasQuePerros/MasQuePerrosFront/src/app/app.module.ts
@@ -2,21 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { HttpClientModule } from '@angular/common/http';
+import { ItemsComponent } from './items/items.component';
+import { ItemService } from './items/item.service';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/productos', pathMatch: 'full'},
+  {path: 'productos', component: ItemsComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [ItemService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ MasQuePerros/MasQuePerrosFront/src/app/items/item.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Item } from './item';
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService {
+
+  private urlEndPoint: string = 'http://localhost:8081/api/users';
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getItems(): Observable<Item[]> {
+    return this.http.get<Item[]>(this.urlEndPoint);
+  }
+}
