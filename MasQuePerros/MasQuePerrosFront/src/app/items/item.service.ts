b32bece06ce99c91bd8cc7349e6ce8207b378262
MarquitosV12 <marcospastoriza12@gmail.com>
2021-05-25 18:10:51 +0200

    Update item.service.ts
    agregado método para recoger item por id en el front en la clase item.service

--- MasQuePerros/MasQuePerrosFront/src/app/items/item.service.ts
+++ MasQuePerros/MasQuePerrosFront/src/app/items/item.service.ts
@@ -1,8 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Item } from './item';
+import swal from 'sweetalert2';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -15,4 +18,15 @@ export class ItemService {
   getItems(): Observable<Item[]> {
     return this.http.get<Item[]>(this.urlEndPoint);
   }
+
+  getItem(id: number): Observable<Item>{
+  return this.http.get<Item>(`${this.urlEndPoint}/${id}`).pipe(
+    catchError(e => {
+      this.router.navigate(['/items/' +id ]);
+      console.error(e.error.mensaje);
+      swal.fire('Error al recuperar el cliente', e.error.mensaje, 'error');
+      return throwError(e);
+    })
+  );
+}
 }
