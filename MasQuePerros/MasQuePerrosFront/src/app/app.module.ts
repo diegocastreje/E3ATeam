ff53847524b87ab03cce1e145c4e4d73fbc0ddbe
leandrotd <leandrotulian45@gmail.com>
2021-05-25 11:08:23 +0200

    Agregada la ruta a users

--- MasQuePerros/MasQuePerrosFront/src/app/app.module.ts
+++ MasQuePerros/MasQuePerrosFront/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
@@ -13,6 +12,7 @@ import { ItemService } from './items/item.service';
 
 const routes: Routes = [
   {path: '', redirectTo: '/items', pathMatch: 'full'},
+  {path: '', redirectTo: '/users', pathMatch: 'full'},
   {path: 'items', component: ItemsComponent}
 ];
 
