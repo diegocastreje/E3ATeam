be080134564673fe189399eb8fe66717afd9be1b
Niqui <hectorniqui96@gmail.com>
2021-05-25 13:00:59 +0200

    preparando cambiar de rama

--- MasQuePerros/MasQuePerrosFront/src/app/users/form.component.ts
+++ MasQuePerros/MasQuePerrosFront/src/app/users/form.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { User } from './user';
 import { Role } from './role';
 import { PaymentMethod } from './payment-method';
+import { UserService } from './user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form',
@@ -11,12 +13,15 @@ export class FormComponent implements OnInit {
 
   private title: string = "Create user";
 
-  constructor() { }
+  constructor(public userService : UserService,
+  public router:Router) { }
 
   ngOnInit(): void {
   }
 
   public create():void{
-
+    this.userService.create(this.user).subscribe(
+      reponse => this.router.navigate(['/users'])
+    )
   }
 }
