e795c6c6e903ebfa025ed8454b8678ec44ee2e06
Zeon <diegocastreje11@gmail.com>
2021-05-26 17:11:20 +0100

    Borrado enlace registro

--- MasQuePerros/MasQuePerrosFront/src/app/login/login.component.ts
+++ MasQuePerros/MasQuePerrosFront/src/app/login/login.component.ts
@@ -8,7 +8,6 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   title: string = "Más Que Perros™";
-  symbol: string = 'TM';
   text1: string = "En esta tienda encontrarás todos los productos para tu mascota.";
   text2: string = "Especialistas en alimentación para perros";
 
