90befb565987c3ece3d015c2404523350c326eb0
francisco.novo <fnovor@fpcoruna.afundacion.org>
2021-05-26 10:53:53 +0200

    Finalzida clase UserService.java

--- MasQuePerros/MasQuePerrosBack/src/main/java/com/e3a/models/services/UserService.java
+++ MasQuePerros/MasQuePerrosBack/src/main/java/com/e3a/models/services/UserService.java
@@ -10,12 +10,16 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService{
+
+    private Logger logger = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
     private IUserDao userDao;
@@ -26,7 +30,12 @@ public class UserService implements UserDetailsService{
 
         User user = userDao.findByUsername(username);
 
-        List<GrantedAuthority> authorities = user.getRole().stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+        if (user == null) {
+            logger.error("Login error: User "+username+" does not exists");
+            throw new UsernameNotFoundException("Login error: User " + username + " does not exists");
+        }
+
+        List<GrantedAuthority> authorities = user.getRole().stream().map(role -> new SimpleGrantedAuthority(role.getName())).peek(authority -> logger.info("Role: " + authority.getAuthority())).collect(Collectors.toList());
 
         return new org.springframework.security.core.userdetails.User(username, user.getPassword(), user.getEnabled(), true, true, true, authorities);
     }
