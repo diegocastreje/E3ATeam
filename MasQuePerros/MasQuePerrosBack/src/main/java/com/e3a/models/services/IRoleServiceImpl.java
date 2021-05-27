73d2280a54db693578558fade4f538af98b46780
francisco.novo <fnovor@fpcoruna.afundacion.org>
2021-05-27 13:06:57 +0200

    Modificaciones de última hora

--- MasQuePerros/MasQuePerrosBack/src/main/java/com/e3a/models/services/IRoleServiceImpl.java
+++ MasQuePerros/MasQuePerrosBack/src/main/java/com/e3a/models/services/IRoleServiceImpl.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.e3a.models.dao.IRoleDao;
 import com.e3a.models.entity.Role;
+
 
 @Service
 public class IRoleServiceImpl implements IRoleService{
@@ -17,6 +19,7 @@ public class IRoleServiceImpl implements IRoleService{
 	
 
 	@Override
+	@Transactional(readOnly=true)
 	public List<Role> findAllRoles() {
 		
 		return roleDao.findAll();
@@ -24,6 +27,7 @@ public class IRoleServiceImpl implements IRoleService{
 
 
 	@Override
+	@Transactional (readOnly=true)
 	public List<Role> findByName(String term) {
 		// TODO Auto-generated method stub
 		return roleDao.findByName(term);
