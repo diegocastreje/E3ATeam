f599e69244988406224e559de76be074d436e364
francisco.novo <fnovor@fpcoruna.afundacion.org>
2021-05-28 10:40:12 +0200

    Creada la clase de servicio AuthService y método login()

---
+++ MasQuePerros/MasQuePerrosFront/src/app/users/auth.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { URLSearchParams } from 'url';
+import { User } from './user';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  login(user:User): Observable<any>{
+  
+    const urlEndpoint = 'http://localhost8081/oauth/token';
+  
+    const credentials = btoa('MasQuePerrosFront' + ':' + '12345');
+  
+    const httpHeaders = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded',
+     'Authorization': 'Basic ' + credentials});
+  
+    let params = new URLSearchParams();
+  
+    params.set('grant_type', 'password');
+  
+    params.set('email', user.email);
+  
+    params.set('password', user.password);
+  
+    return this.http.post<any>(urlEndpoint, params, {headers: httpHeaders});
+  }
+
+}
