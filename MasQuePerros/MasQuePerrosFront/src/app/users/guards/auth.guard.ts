0c07d80c69b8c427f07bac9726b241443b711f80
leandrotd <leandrotulian45@gmail.com>
2021-06-01 12:55:43 +0200

    Arreglada el login pero con fallos aparte

--- MasQuePerros/MasQuePerrosFront/src/app/users/guards/auth.guard.ts
+++ MasQuePerros/MasQuePerrosFront/src/app/users/guards/auth.guard.ts
@@ -1,59 +1,56 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  Router,
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-
-  constructor(private authService: AuthService, private router: Router) {
-
-  }
+  constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (this.authService.isAuthenticated()) {
+      if (this.isTokenExpired()) {
+        this.authService.logout();
 
-      if(this.authService.isAuthenticated()) {
-        
-        if(this.isTokenExpired()) {
+        this.router.navigate(['/login']);
 
-          this.authService.logout();
-
-          this.router.navigate(['/login']);
-
-          return false;
-
-        }
-
-        return true;
-
+        return false;
       }
 
-      this.router.navigate(['/login']);
+      return true;
+    }
+
+    this.router.navigate(['/login']);
 
     return false;
-
   }
 
-  isTokenExpired(): boolean{
-
+  isTokenExpired(): boolean {
     let token = this.authService.token;
 
-    let payload = this.authService.importTokenData(token);
+    let payload = this.authService.getTokenData(token || '');
 
     let now = new Date().getTime() / 1000;
 
-    if(payload.exp < now) {
-
+    if (payload.exp < now) {
       return true;
-
     }
 
     return false;
-
   }
-
 }
