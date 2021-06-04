703e00597c605a0ab3431c281dfda9c84190a232
francisco.novo <fnovor@fpcoruna.afundacion.org>
2021-06-04 14:47:35 +0200

    Más cambios

--- MasQuePerros/MasQuePerrosFront/src/app/app.module.ts
+++ MasQuePerros/MasQuePerrosFront/src/app/app.module.ts
@@ -31,7 +31,8 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'items', component: ItemsComponent },
-  { path: 'items/:id', component: FormComponent },
+  {path: 'items/form', component: ItemFormComponent, canActivate: [AuthGuard, RoleGuard], data: {role: 'ROLE_ADMIN' || 'ROLE_CLERK'}},
+  {path: 'items/form/:id', component: ItemFormComponent, canActivate: [AuthGuard, RoleGuard], data: {role: 'ROLE_ADMIN' || 'ROLE_CLERK'}},
   { path: 'users', component: UserComponent },
   {
     path: 'users/form',
